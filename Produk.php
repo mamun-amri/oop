d6e5ea8a426155b48138f390afa4ff0dcdfd4abb
tafrij <mamunamri97@gmail.com>
2019-11-23 17:13:37 +0700

    menambahkan construct dan overriding

--- Produk.php
+++ Produk.php
@@ -3,27 +3,72 @@ class Produk
 {
     // property
     public  $judul,
-        $penerbit   = "penerbit",
-        $penulis    = "penulis",
-        $harga      = 0;
+        $penerbit,
+        $penulis,
+        $harga,
+        $halaman,
+        $waktuMain;
 
-    public function __construct($judul = "judul", $penerbit = "penerbit", $penulis = "penulis", $harga = 0)
+    public function __construct($judul = "judul", $penerbit = "penerbit", $penulis = "penulis", $harga = 0, $halaman = 0, $waktuMain = 0)
     {
         $this->judul    = $judul;
         $this->penerbit = $penerbit;
         $this->penulis  = $penulis;
         $this->harga    = $harga;
+        $this->halaman  = $halaman;
+        $this->waktuMain = $waktuMain;
     }
 
     public function getLabel()
     {
         return "$this->penulis, $this->penerbit";
     }
+
+    public function getInfoProduk()
+    {
+        $str = "{$this->judul}|{$this->getLabel()} (Rp.{$this->harga})";
+        return $str;
+    }
 }
 
-$produk1 = new Produk("Naruto", "Masahi", "Shunen", 30000);
-$produk2 = new Produk("Good Of War", "Ceo", "Cantik", 250000);
+// contoh object type
+class cetakProduk
+{
+    public function cetak(Produk $produk)
+    {
+        $str = "{$produk->judul}|{$produk->getLabel()} (Rp.{$produk->harga})";
+        return $str;
+    }
+}
 
-echo "Komik : " . $produk1->getLabel();
-echo "<hr>";
-echo "Game : " . $produk2->getLabel();
+class Komik extends Produk
+{
+    public function getInfoProduk()
+    {
+        $str = "Komik : " . parent::getInfoProduk() . " - {$this->halaman} Halaman";
+        return $str;
+    }
+}
+
+class Game extends Produk
+{
+    public function getInfoProduk()
+    {
+        /* parent::getInfoProduk() contoh overriding */
+        $str = "Game : " . parent::getInfoProduk() . " ~ {$this->waktuMain} Jam";
+        return $str;
+    }
+}
+
+
+$produk1 = new Komik("Naruto", "Masahi", "Shunen", 30000, 100);
+$produk2 = new Game("Good Of War", "Ceo", "Cantik", 250000, 0, 5);
+
+echo $produk1->getInfoProduk();
+echo '<br>';
+echo $produk2->getInfoProduk();
+echo '<br>';
+// example Object type
+echo '<hr>';
+$cetakProduk = new cetakProduk();
+echo $cetakProduk->cetak($produk1);
